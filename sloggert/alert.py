00d29b2e502d5b62001f921a03026bf3b2cd4cb4
Johan Nestaas <johannestaas@gmail.com>
2016-07-22 02:53:02 -0700

    print works

--- sloggert/alert.py
+++ sloggert/alert.py
@@ -10,29 +10,32 @@ from .db import MessageDB
 
 class Alerter(MessageDB):
 
-    def __init__(self, **kwargs):
-        super(Alerter, self).__init__(**kwargs)
+    def __init__(self, *args, **kwargs):
+        super(Alerter, self).__init__(*args, **kwargs)
         self.selections = {}
-        self._select = self.select
 
     def add_selection(self, name, field):
         self.selections[name] = field
 
-    def select(self, name=None):
+    def query(self, name=None):
         if name is not None:
-            return self._select(self.selections[name].build_query())
+            return self.select(self.selections[name])
         else:
             messages = {}
             for name, field in self.selections.items():
-                messages[name] = self._select(field.build_query())
+                messages[name] = self.select(field)
             return messages
 
     def print(self, name=None):
         if name is None:
             for name in self.selections:
                 print('=== {} ==='.format(name))
-                for msg in self.select(name=name):
+                for msg in self.query(name=name):
+                    msg['_id'] = str(msg['_id'])
+                    msg['_datetime_'] = msg['_datetime_'].isoformat()
                     print('  {}'.format(json.dumps(msg)))
         else:
-            for msg in self.select(name=name):
+            for msg in self.query(name=name):
+                msg['_id'] = str(msg['_id'])
+                msg['_datetime_'] = msg['_datetime_'].isoformat()
                 print(json.dumps(msg))
